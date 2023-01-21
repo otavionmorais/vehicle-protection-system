f950a3320f5b54fb0f1ade2b87f3e376ed93becc
Otávio Morais <otavio.n.morais@hotmail.com>
2023-01-21 19:27:22 -0300

    feat: creating client model

---
+++ src/clients/clients.model.ts
@@ -0,0 +1,19 @@
+export class Client {
+  id: string;
+
+  document: string;
+
+  name: string;
+
+  email: string;
+
+  phone: string;
+
+  birth_date: Date;
+
+  password: string;
+
+  created_at: Date;
+
+  updated_at: Date;
+}
