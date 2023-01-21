6f02e495b4830fabddede242357c02b6fb3499b5
Otávio Morais <otavio.n.morais@hotmail.com>
2023-01-21 20:26:02 -0300

    feat: creating service and repository structures

---
+++ src/clients/structures.ts
@@ -0,0 +1,31 @@
+import { Client } from './clients.model';
+
+export interface ICreateClientDTO {
+  document: string;
+  name: string;
+  email: string;
+  phone: string;
+  birth_date: Date;
+  password: string;
+}
+
+export interface IFindClientDTO {
+  id?: string;
+  document?: string;
+  email?: string;
+}
+
+export interface IClientsService {
+  create(data: ICreateClientDTO): Promise<Client>;
+  update(id: string, data: Partial<ICreateClientDTO>): Promise<Client>;
+  delete(id: string): Promise<void>;
+  find(id: IFindClientDTO): Promise<Client>;
+  authenticate(email: string, password: string): Promise<Client>;
+}
+
+export interface IClientsRepository {
+  create(data: ICreateClientDTO): Promise<Client>;
+  update(id: string, data: Partial<ICreateClientDTO>): Promise<Client>;
+  delete(id: string): Promise<void>;
+  find(id: IFindClientDTO): Promise<Client>;
+}
