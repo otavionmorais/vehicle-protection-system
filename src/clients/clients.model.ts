714962e01b6bf8b78ffdea5ab0c0a41a7457bede
Otávio Morais <otavio.n.morais@hotmail.com>
2023-01-22 19:53:51 -0300

    feat: adding typeorm props to model

--- src/clients/clients.model.ts
+++ src/clients/clients.model.ts
@@ -1,23 +1,42 @@
 import { Exclude } from 'class-transformer';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
+@Entity({
+  name: 'clients',
+})
 export class Client {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   document: string;
 
+  @Column()
   name: string;
 
+  @Column()
   email: string;
 
+  @Column()
   phone: string;
 
-  birth_date: Date;
+  @Column()
+  birth_date: string;
 
   @Exclude()
+  @Column()
   password: string;
 
+  @CreateDateColumn({ type: 'timestamp without time zone' })
   created_at: Date;
 
+  @UpdateDateColumn({ type: 'timestamp without time zone' })
   updated_at: Date;
 
   build<K extends keyof Omit<Client, 'build'>>(
