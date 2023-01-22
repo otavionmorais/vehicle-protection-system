c3fdc23db546c471bcc12d5254baa30653e6a8a6
Otávio Morais <otavio.n.morais@hotmail.com>
2023-01-22 19:54:30 -0300

    feat: creating client in database to test typeorm

--- src/clients/clients.repository.ts
+++ src/clients/clients.repository.ts
@@ -4,12 +4,26 @@ import {
   ICreateClientDTO,
   IFindClientDTO,
 } from './structures';
-import { injectable } from 'tsyringe';
+import { dataSource } from '../database';
+import { CustomError, ErrorIdentifier } from '../errors';
 
-@injectable()
 export class ClientsRepository implements IClientsRepository {
+  private repository = dataSource.getRepository(Client);
+
   async create(data: ICreateClientDTO): Promise<Client> {
-    throw new Error('Method not implemented.');
+    try {
+      const entity = this.repository.create(data);
+      const savedRegister = await this.repository.save(entity);
+
+      return savedRegister;
+    } catch (error) {
+      console.error('Database error: ', error);
+      throw new CustomError(
+        'An unexpected error occurred. Try again later.',
+        ErrorIdentifier.INTERNAL_SERVER_ERROR,
+        500,
+      );
+    }
   }
 
   async update(id: string, data: Partial<ICreateClientDTO>): Promise<Client> {
