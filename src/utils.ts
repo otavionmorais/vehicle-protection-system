f3b240eedd539766e825ba24f8032ba0ad19e85c
Otávio Morais <otavio.n.morais@hotmail.com>
2023-01-22 19:53:30 -0300

    feat: adding env variables verification and db

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { ResponseToolkit } from '@hapi/hapi';
+import { parse } from 'date-fns';
 
 export async function treatRequest(fn: Promise<unknown>, res: ResponseToolkit) {
   try {
@@ -7,10 +8,34 @@ export async function treatRequest(fn: Promise<unknown>, res: ResponseToolkit) {
   } catch (err) {
     return res
       .response({
-        statusCode: err.code || 500,
-        error: err.message,
+        statusCode: err.httpCode || 500,
+        error: err.identifier || err.message,
         message: err.message,
       })
-      .code(err.code || 500);
+      .code(err.httpCode || 500);
+  }
+}
+
+export function validateEnvironmentVariables() {
+  const requiredVariables = ['PORT', 'DATABASE_URL'];
+
+  for (const variable of requiredVariables) {
+    if (!process.env[variable]) {
+      throw new Error(`Missing environment variable: ${variable}`);
+    }
+  }
+}
+
+export function isValidDateWithoutTime(dateString: string): boolean {
+  try {
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+
+    if (!dateRegex.test(dateString)) {
+      return false;
+    }
+
+    return !!parse(dateString, 'yyyy-MM-dd', new Date()).getTime();
+  } catch {
+    return false;
   }
 }
